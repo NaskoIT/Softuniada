66a352ae4a47c27f3ad0ff756925cefa258911c3
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-01 22:05:16 +0200

    Solve fifth and sixth tasks from softuniada

---
+++ Softuniada 2018/Asteroids/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] fieldDimensoins = command.Split('x');
+                int n = int.Parse(fieldDimensoins[0]);
+
+                char[][] matrix = new char[n][];
+                for (int row = 0; row < n; row++)
+                {
+                    matrix[row] = Console.ReadLine().ToCharArray();
+                }
+
+                SortedDictionary<int, int> areasByCount = FindAsteroids(matrix);
+
+                foreach (var areaByCount in areasByCount)
+                {
+                    sb.AppendLine($"{areaByCount.Value}x{areaByCount.Key}");
+                }
+
+                sb.AppendLine($"Total: {areasByCount.Values.Sum()}");
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+
+        private static SortedDictionary<int, int> FindAsteroids(char[][] matrix)
+        {
+            bool[][] visited = new bool[matrix.Length][];
+            for (int row = 0; row < visited.Length; row++)
+            {
+                visited[row] = new bool[matrix[row].Length];
+            }
+
+            SortedDictionary<int, int> areasByCount = new SortedDictionary<int, int>(Comparer<int>.Create((x, y) => y - x));
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == '1' && !visited[row][col])
+                    {
+                        int area = CalculateAsteroidsArea(row, col, matrix, visited);
+                        if (!areasByCount.ContainsKey(area))
+                        {
+                            areasByCount[area] = 0;
+                        }
+
+                        areasByCount[area]++;
+                    }
+                }
+            }
+
+            return areasByCount;
+        }
+
+        public static int CalculateAsteroidsArea(int row, int col, char[][] matrix, bool[][] visited)
+        {
+            if (!IsInBounds(matrix, row, col) || visited[row][col] || matrix[row][col] == '0')
+            {
+                return 0;
+            }
+
+            visited[row][col] = true;
+
+            return 1 + CalculateAsteroidsArea(row + 1, col, matrix, visited) +
+                CalculateAsteroidsArea(row - 1, col, matrix, visited) +
+                CalculateAsteroidsArea(row, col + 1, matrix, visited) +
+                CalculateAsteroidsArea(row, col - 1, matrix, visited);
+        }
+
+        private static bool IsInBounds(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+    }
+
+    public class Cell
+    {
+        public Cell(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; private set; }
+
+        public int Col { get; private set; }
+    }
+}
---
+++ Softuniada 2018/CircumscribedCircle/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace CircumscribedCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shapesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < shapesCount; i++)
+            {
+                Circle circle = ReadCircle();
+                Triangle triangle = ReadTriangle();
+               
+                bool isCenterInsideTriangle = IsPointInsideTriangle(circle.Center, triangle);
+                bool isCircumscribed = circle.IsCircumscribed(triangle);
+                string circumscribed = isCircumscribed ? "circumscribed" : "not circumscribed";
+                string position = isCenterInsideTriangle ? "inside" : "outside";
+
+                Console.WriteLine($"The circle is {circumscribed} and the center is {position}.");
+            }
+        }
+
+        private static Triangle ReadTriangle()
+        {
+            double[] tokens = Console.ReadLine().Substring(9).Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            return new Triangle
+            {
+                A = new Point
+                {
+                    X = tokens[0],
+                    Y = tokens[1]
+                },
+                B = new Point
+                {
+                    X = tokens[2],
+                    Y = tokens[3]
+                },
+                C = new Point
+                {
+                    X = tokens[4],
+                    Y = tokens[5]
+                }
+            };
+        }
+
+        private static Circle ReadCircle()
+        {
+            double[] tokens = Console.ReadLine().Substring(7).Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            return new Circle
+            {
+                Center = new Point
+                {
+                    X = tokens[0],
+                    Y = tokens[1]
+                },
+                Radius = tokens[2]
+            };
+        }
+
+        private static double CalculateArea(Point a, Point b, Point c)
+        {
+                return Math.Abs((a.X * (b.Y - c.Y) +
+                                 b.X * (c.Y - a.Y) +
+                                 c.X * (a.Y - b.Y)) / 2.0);
+        }
+
+        private static bool IsPointInsideTriangle(Point point, Triangle triangle)
+        {
+            double mainTriangleArea = CalculateArea(triangle.A, triangle.B, triangle.C);
+            double firstSubTriangleArea = CalculateArea(triangle.A, triangle.B, point);
+            double secondSubTriangleArea = CalculateArea(triangle.A, triangle.C, point);
+            double thirdSubTriangleArea = CalculateArea(triangle.B, triangle.C, point);
+
+            return Math.Abs(mainTriangleArea - (firstSubTriangleArea + secondSubTriangleArea + thirdSubTriangleArea)) <= 0.01;
+        }
+    }
+
+    public class Circle
+    {
+        public double Radius { get; set; }
+
+        public Point Center { get; set; }
+
+        public bool IsCircumscribed(Triangle triangle)
+        {
+            return Math.Abs(triangle.A.CalculateDistance(Center) - Radius) <= 0.01 &&
+                   Math.Abs(triangle.B.CalculateDistance(Center) - Radius) <= 0.01 &&
+                   Math.Abs(triangle.C.CalculateDistance(Center) - Radius) <= 0.01;
+        }
+    }
+
+    public class Triangle
+    {
+        public Point A { get; set; }
+
+        public Point B { get; set; }
+
+        public Point C { get; set; }
+    }
+
+    public struct Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double CalculateDistance(Point other)
+        {
+            double firstSide = Math.Abs(X - other.X);
+            double secondSide = Math.Abs(Y - other.Y);
+            return Math.Sqrt(firstSide * firstSide + secondSide * secondSide);
+        }
+    }
+}
