fccff0e600dd8557d2de4e2381d317d810174c23
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-02 20:18:00 +0200

    Softunida seventh and eighth  problems

---
+++ Softuniada 2018/Car/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] speedChanges = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int initialSpeed = int.Parse(Console.ReadLine());
+            int maxSpeed = int.Parse(Console.ReadLine());
+            int minSpeed = 0;
+
+            bool[,] possibleSpeeds = new bool[speedChanges.Length + 1, maxSpeed + 1];
+            possibleSpeeds[0, initialSpeed] = true;
+
+            for (int row = 1; row < possibleSpeeds.GetLength(0); row++)
+            {
+                for (int currentSpeed = 0; currentSpeed < possibleSpeeds.GetLength(1); currentSpeed++)
+                {
+                    int currentSpeedChange = speedChanges[row - 1];
+
+                    if (possibleSpeeds[row - 1, currentSpeed])
+                    {
+                        if (currentSpeed - currentSpeedChange >= minSpeed)
+                        {
+                            possibleSpeeds[row, currentSpeed - currentSpeedChange] = true;
+                        }
+
+                        if (currentSpeed + currentSpeedChange <= maxSpeed)
+                        {
+                            possibleSpeeds[row, currentSpeed + currentSpeedChange] = true;
+                        }
+                    }
+                }
+            }
+
+            int maxPossibleSpeed = -1;
+
+            for (int possibleSpeed = possibleSpeeds.GetLength(1) - 1; possibleSpeed >= 0; possibleSpeed--)
+            {
+                if (possibleSpeeds[possibleSpeeds.GetLength(0) - 1, possibleSpeed])
+                {
+                    maxPossibleSpeed = possibleSpeed;
+                    break;
+                }
+            }
+
+            Console.WriteLine(maxPossibleSpeed);
+        }
+    }
+}
---
+++ Softuniada 2018/Logistics/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Logistics
+{
+    class Program
+    {
+        private const int VanRepairSum = 50;
+        private static readonly Dictionary<int, List<Package>> packages = new Dictionary<int, List<Package>>();
+        private static HashSet<int> accidents = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            int amountOfPackages = int.Parse(Console.ReadLine());
+            int maxDeadline = 0;
+            ; for (int i = 1; i <= amountOfPackages; i++)
+            {
+                var packageParts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var package = new Package
+                {
+                    Id = i,
+                    Price = packageParts[0],
+                    Deadline = packageParts[1]
+                };
+
+                if (maxDeadline < package.Deadline)
+                {
+                    maxDeadline = package.Deadline;
+                }
+                if (!packages.ContainsKey(package.Deadline))
+                {
+                    packages[package.Deadline] = new List<Package>();
+                }
+
+                packages[package.Deadline].Add(package);
+            }
+
+            string crachesInput = Console.ReadLine();
+            if (crachesInput != "none")
+            {
+                accidents = new HashSet<int>(crachesInput.Split().Select(int.Parse));
+            }
+
+            var sortedPackages = new SortedSet<Package>();
+            var bestPackages = new List<int>();
+            long total = 0;
+
+            for (int deadline = maxDeadline; deadline >= 1; deadline--)
+            {
+                if (packages.ContainsKey(deadline))
+                {
+                    foreach (var package in packages[deadline])
+                    {
+                        sortedPackages.Add(package);
+                    }
+                }
+
+                Package bestPackage = null;
+                if (accidents.Contains(deadline))
+                {
+                    bestPackage = sortedPackages.Min;
+                    total -= VanRepairSum;
+                    total -= bestPackage.Price;
+                }
+                else
+                {
+                    bestPackage = sortedPackages.Max;
+                    total += bestPackage.Price;
+                }
+
+                sortedPackages.Remove(bestPackage);
+                bestPackages.Add(bestPackage.Id);
+            }
+
+            Console.WriteLine(total);
+            bestPackages.Reverse();
+            Console.WriteLine(string.Join(" ", bestPackages));
+        }
+    }
+
+    public class Package : IComparable<Package>
+    {
+        public int Id { get; set; }
+
+        public int Price { get; set; }
+
+        public int Deadline { get; set; }
+
+        public int CompareTo(Package other)
+        {
+            int compare = Price.CompareTo(other.Price);
+            if (compare == 0)
+            {
+                compare = Deadline.CompareTo(other.Deadline);
+                if (compare == 0)
+                {
+                    compare = Id.CompareTo(other.Id);
+                }
+            }
+
+            return compare;
+        }
+    }
+}
