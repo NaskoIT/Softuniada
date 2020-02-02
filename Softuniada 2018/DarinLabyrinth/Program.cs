fde052156f4a4a87262852541044c485841c8db2
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-03 00:41:26 +0200

    DarinLabirynth half correct solution

---
+++ Softuniada 2018/DarinLabyrinth/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DarinLabyrinth
+{
+    class Program
+    {
+        private static List<int>[] graph;
+        private static int[] depths;
+        private static int[] lowpoints;
+        private static bool[] visited;
+        private static int?[] parent;
+        private static HashSet<int> articulationPoints;
+
+        static void Main(string[] args)
+        {
+            int numberOfRooms = int.Parse(Console.ReadLine());
+            int numberOfTunels = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[numberOfRooms];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            for (int i = 0; i < numberOfTunels; i++)
+            {
+                int[] connection = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int startNode = connection[0];
+                int endNode = connection[1];
+                graph[startNode].Add(endNode);
+                graph[endNode].Add(startNode);
+            }
+
+            var shortestPath = FindShortedPath(0, graph.Length - 1);
+            FindArticulationPoints();
+
+            var keyRooms = shortestPath.Where(room => articulationPoints.Contains(room));
+            long sum = 0;
+            foreach (var room in keyRooms)
+            {
+                sum += room;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(string.Join("->", keyRooms));
+        }
+
+        public static void FindArticulationPoints()
+        {
+            depths = new int[graph.Length];
+            lowpoints = new int[graph.Length];
+            visited = new bool[graph.Length];
+            parent = new int?[graph.Length];
+            articulationPoints = new HashSet<int>();
+
+            if (graph.Length > 0)
+            {
+                FindArticulationPoints(0, 1);
+            }
+        }
+
+        private static void FindArticulationPoints(int node, int depth)
+        {
+            visited[node] = true;
+            lowpoints[node] = depth;
+            depths[node] = depth;
+            int childCount = 0;
+            bool isArticulation = false;
+
+            foreach (var childNode in graph[node])
+            {
+                if (!visited[childNode])
+                {
+                    parent[childNode] = node;
+                    FindArticulationPoints(childNode, depth + 1);
+                    childCount += 1;
+                    if (lowpoints[childNode] >= depths[node])
+                    {
+                        isArticulation = true;
+                    }
+                    lowpoints[node] = Math.Min(lowpoints[node], lowpoints[childNode]);
+                }
+                else if (childNode != parent[node])
+                {
+                    lowpoints[node] = Math.Min(lowpoints[node], depths[childNode]);
+                }
+            }
+
+            if ((parent[node] != null && isArticulation) || (parent[node] == null && childCount > 1))
+            {
+                articulationPoints.Add(node);
+            }
+        }
+
+        private static IEnumerable<int> FindShortedPath(int sourceNode, int destinationNode)
+        {
+            int[] parents = new int[graph.Length];
+            int[] minDistances = new int[graph.Length];
+            for (int i = 0; i < minDistances.Length; i++)
+            {
+                minDistances[i] = int.MaxValue;
+                parents[i] = -1;
+            }
+
+            minDistances[sourceNode] = 0;
+            bool[] visited = new bool[minDistances.Length];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(sourceNode);
+            visited[sourceNode] = true;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+                if (node == destinationNode)
+                {
+                    break;
+                }
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited[child])
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+
+                        int currentDistance = minDistances[node] + 1;
+                        if (minDistances[child] > currentDistance)
+                        {
+                            minDistances[child] = currentDistance;
+                            parents[child] = node;
+                        }
+                    }
+                }
+            }
+
+            if (minDistances[destinationNode] <= 0)
+            {
+                return null;
+            }
+
+            Stack<int> shortestPath = new Stack<int>();
+            int currentNode = destinationNode;
+
+            while (parents[currentNode] != -1)
+            {
+                currentNode = parents[currentNode];
+                if (currentNode == sourceNode)
+                {
+                    break;
+                }
+
+                shortestPath.Push(currentNode);
+            }
+
+            return shortestPath;
+        }
+    }
+}
