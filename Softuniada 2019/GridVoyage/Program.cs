60c45b8aa452548385f2ba37bb4da49943734c5b
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-13 22:49:38 +0200

    5. Grid Voyage problem project

---
+++ Softuniada 2019/GridVoyage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GridVoyage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
