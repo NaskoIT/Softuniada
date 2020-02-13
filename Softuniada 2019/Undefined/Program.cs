60fdf0037595e3201426d16b056a7461adb61ad0
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-13 22:50:59 +0200

    7. Undefined project problem

---
+++ Softuniada 2019/Undefined/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Undefined
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
