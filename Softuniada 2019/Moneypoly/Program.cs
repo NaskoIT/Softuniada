16a72bf2c93831677af0b95b44cb52b0f103c61a
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-13 22:52:25 +0200

    9. Moneypoly problem project

---
+++ Softuniada 2019/Moneypoly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Moneypoly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
