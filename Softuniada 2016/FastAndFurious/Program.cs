939bf0d53220c8e73f62bdc569a91466ea897323
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-21 22:30:21 +0200

    Solve Fast and Furious task

--- Softuniada 2016/FastAndFurious/Program.cs
+++ Softuniada 2016/FastAndFurious/Program.cs
@@ -1,12 +1,194 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace FastAndFurious
 {
     class Program
     {
+        private static readonly List<Road> roads = new List<Road>();
+        private static double[,] graph;
+        private static readonly Dictionary<string, int> townsByIndex = new Dictionary<string, int>();
+        private static int townsCount = 0;
+        private static readonly Dictionary<string, SortedSet<Record>> driverByRecords = new Dictionary<string, SortedSet<Record>>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadInput();
+
+            BuildGraph();
+
+            List<string> result = new List<string>();
+
+            foreach (var driverByRecord in driverByRecords)
+            {
+                string driver = driverByRecord.Key;
+                Record[] records = driverByRecord.Value.ToArray();
+                bool isSpeeding = false;
+
+                for (int currentIndex = 0; currentIndex < records.Length && !isSpeeding; currentIndex++)
+                {
+                    for (int nextIndex = currentIndex + 1; nextIndex < records.Length; nextIndex++)
+                    {
+                        string firstTown = records[currentIndex].CameraName;
+                        string secondTown = records[nextIndex].CameraName;
+                        TimeSpan actualTimeInHour = records[nextIndex].Time - records[currentIndex].Time;
+                        double bestTimeInHours = DijkstraAlgorithm(graph, townsByIndex[firstTown], townsByIndex[secondTown]);
+
+                        if (bestTimeInHours != double.MaxValue && bestTimeInHours > actualTimeInHour.TotalHours)
+                        {
+                            result.Add(driver);
+                            isSpeeding = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach (var driver in result.OrderBy(x => x))
+            {
+                Console.WriteLine(driver);
+            }
+        }
+
+        private static void BuildGraph()
+        {
+            graph = new double[townsCount, townsCount];
+
+            foreach (var road in roads)
+            {
+                int firstTownIndex = townsByIndex[road.FirstCameraName];
+                int secondTownIndex = townsByIndex[road.SecondCameraName];
+                graph[firstTownIndex, secondTownIndex] = road.TimeInHours;
+                graph[secondTownIndex, firstTownIndex] = road.TimeInHours;
+            }
+        }
+
+        public static double DijkstraAlgorithm(double[,] graph, int sourceNode, int destinationNode)
+        {
+            int elementsCount = graph.GetLength(0);
+            double[] distances = new double[elementsCount];
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = double.MaxValue;
+            }
+
+            distances[sourceNode] = 0;
+
+            bool[] used = new bool[elementsCount];
+            int?[] previous = new int?[elementsCount];
+
+            while (true)
+            {
+                double minDistance = double.MaxValue;
+                int minNode = 0;
+
+                for (int node = 0; node < elementsCount; node++)
+                {
+                    if (!used[node] && distances[node] < minDistance)
+                    {
+                        minDistance = distances[node];
+                        minNode = node;
+                    }
+                }
+
+                if (minDistance == double.MaxValue)
+                {
+                    break;
+                }
+
+                used[minNode] = true;
+
+                for (int node = 0; node < elementsCount; node++)
+                {
+                    double weight = graph[minNode, node];
+
+                    if (weight > 0)
+                    {
+                        double currentMinDistance = distances[minNode] + weight;
+                        if (currentMinDistance < distances[node])
+                        {
+                            distances[node] = currentMinDistance;
+                            previous[node] = minNode;
+                        }
+                    }
+                }
+            }
+
+            return distances[destinationNode];
+        }
+
+        private static void ReadInput()
+        {
+            string command = Console.ReadLine();
+
+            while ((command = Console.ReadLine()) != "Records:")
+            {
+                string[] tokens = command.Split();
+                Road road = new Road
+                {
+                    FirstCameraName = tokens[0],
+                    SecondCameraName = tokens[1],
+                    Distance = double.Parse(tokens[2]),
+                    SpeedLimit = double.Parse(tokens[3])
+                };
+
+                roads.Add(road);
+                AddTown(road.FirstCameraName);
+                AddTown(road.SecondCameraName);
+            }
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] tokens = command.Split();
+                string licensePlateNumber = tokens[1];
+                Record record = new Record
+                {
+                    CameraName = tokens[0],
+                    Time = TimeSpan.Parse(tokens[2], CultureInfo.InvariantCulture)
+                };
+
+                if (!driverByRecords.ContainsKey(licensePlateNumber))
+                {
+                    driverByRecords[licensePlateNumber] = new SortedSet<Record>();
+                }
+
+                driverByRecords[licensePlateNumber].Add(record);
+            }
+        }
+
+        private static void AddTown(string townName)
+        {
+            if (!townsByIndex.ContainsKey(townName))
+            {
+                townsByIndex.Add(townName, townsCount++);
+            }
+        }
+    }
+
+    public class Road
+    {
+        public string FirstCameraName { get; set; }
+
+        public string SecondCameraName { get; set; }
+
+        public double Distance { get; set; }
+
+        public double SpeedLimit { get; set; }
+
+        public double TimeInHours => Distance / SpeedLimit;
+    }
+
+    public class Record : IComparable<Record>
+    {
+        public string CameraName { get; set; }
+
+        public TimeSpan Time { get; set; }
+
+        public int CompareTo(Record other)
+        {
+            return Time.CompareTo(other.Time);
         }
     }
 }
--- Softuniada 2016/PackagingFigures/Program.cs
+++ Softuniada 2016/PackagingFigures/Program.cs
@@ -1,12 +1,241 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PackagingFigures
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var figures = ReadFigures();
+
+            for (int i = 0; i < figures.Count; i++)
+            {
+                Figure firstFigure = figures[i];
+
+                for (int j = 0; j < figures.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    Figure secondFigure = figures[j];
+                    
+                    if (firstFigure is Circle firstCircle)
+                    {
+                        //Check if secind circle is inside first circle
+                        if (secondFigure is Circle secondCircle)
+                        {
+                            if(secondCircle.Radius > firstCircle.Radius)
+                            {
+                                continue;
+                            }
+
+                            int distanceX = Math.Abs(firstCircle.CenterX - secondCircle.CenterX);
+                            int distanceY = Math.Abs(firstCircle.CenterY - secondCircle.CenterY);
+                            if(distanceX > firstCircle.Radius || distanceY > firstCircle.Radius)
+                            {
+                                continue;
+                            }
+
+                            long distanceBetweenCentersSquare = (long)distanceY * distanceY + (long)distanceX * distanceX;
+                            long delatRSquare = (long)(firstCircle.Radius - secondCircle.Radius) * (firstCircle.Radius - secondCircle.Radius);
+                        
+                            if(distanceBetweenCentersSquare <= delatRSquare)
+                            {
+                                firstCircle.Children.Add(secondCircle);
+                            }
+                        }
+                        else if(secondFigure is Rectangle rectangle)
+                        {
+                            //Check is rectangle is inside circle
+                            int distanceX = Math.Max(firstCircle.CenterX - rectangle.LeftX, rectangle.RightX - firstCircle.CenterX);
+                            int distanceY = Math.Max(rectangle.TopY - firstCircle.CenterY, firstCircle.CenterY - rectangle.BottomY);
+                            long distance = ((long)distanceX * distanceX) + ((long)distanceY * distanceY);
+                            bool isInside = (long)firstCircle.Radius * firstCircle.Radius >= distance;
+                            if (isInside)
+                            {
+                                firstCircle.Children.Add(rectangle);
+                            }
+                        }
+                    }
+                    else if (firstFigure is Rectangle rectangle)
+                    {
+                        //Check if second circle is inside rectangle
+                        if (secondFigure is Circle circle)
+                        {
+                            bool inLeft = circle.CenterX - rectangle.LeftX >= circle.Radius;
+                            bool inRight = rectangle.RightX - circle.CenterX >= circle.Radius;
+                            bool inTop = rectangle.TopY - circle.CenterY >= circle.Radius;
+                            bool inBottom = circle.CenterY - rectangle.BottomY >= circle.Radius;
+
+                            if(inLeft && inRight && inTop && inBottom)
+                            {
+                                rectangle.Children.Add(circle);
+                            }
+                        }
+                        else if (secondFigure is Rectangle secondRectangle)
+                        {
+                            //Check if second rectangle is inside first rectangle
+                            if(secondRectangle.RightX > rectangle.RightX || 
+                               secondRectangle.LeftX < rectangle.LeftX ||
+                               secondRectangle.TopY > rectangle.TopY ||
+                               secondRectangle.BottomY < rectangle.BottomY)
+                            {
+                                continue;
+                            }
+
+                            rectangle.Children.Add(secondRectangle);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < figures.Count; i++)
+            {
+                Dfs(figures[i]);
+            }
+
+            Figure top = figures.OrderByDescending(f => f.Depth).ThenBy(x => x.Name).First();
+            int depth = top.Depth;
+            List<string> targetFigures = new List<string>();
+
+            while(top != null && depth > 0)
+            {
+                targetFigures.Add(top.Name);
+                depth -= 1;
+                top = top.Successor;
+            }
+
+            Console.WriteLine(string.Join(" < ", targetFigures));
+        }
+
+        private static int Dfs(Figure figure)
+        {
+            if(figure.Depth > 0)
+            {
+                return figure.Depth;
+            }
+
+            figure.Depth = 1;
+            figure.Successor = null;
+
+            foreach (var child in figure.Children)
+            {
+                int currentDepth = Dfs(child) + 1;
+                if(currentDepth > figure.Depth || 
+                  (currentDepth == figure.Depth && 
+                  child.Name.CompareTo(figure.Successor.Name) < 0))
+                {
+                    figure.Depth = currentDepth;
+                    figure.Successor = child;
+                }
+            }
+
+            return figure.Depth;
+        }
+
+        private static List<Figure> ReadFigures()
+        {
+            List<Figure> figures = new List<Figure>();
+            string line = Console.ReadLine();
+
+            while (line != "End")
+            {
+                string[] parameters = line.Split();
+                string type = parameters[0];
+                string name = parameters[1];
+
+                int x1 = 0;
+                int y1 = 0;
+                int x2 = 0;
+                int y2 = 0;
+                Figure figure = null;
+
+                switch (type)
+                {
+                    case "rectangle":
+                        x1 = int.Parse(parameters[2]);
+                        y1 = int.Parse(parameters[3]);
+                        x2 = int.Parse(parameters[4]);
+                        y2 = int.Parse(parameters[5]);
+                        figure = new Rectangle(name, x1, y1, x2, y2);
+                        break;
+                    case "square":
+                        x1 = int.Parse(parameters[2]);
+                        y1 = int.Parse(parameters[3]);
+                        int side = int.Parse(parameters[4]);
+                        x2 = x1 + side;
+                        y2 = y1 - side;
+                        figure = new Rectangle(name, x1, y1, x2, y2);
+                        break;
+                    case "circle":
+                        int centerX = int.Parse(parameters[2]);
+                        int centerY = int.Parse(parameters[3]);
+                        int radius = int.Parse(parameters[4]);
+                        figure = new Circle(name, centerX, centerY, radius);
+                        break;
+                }
+
+                figures.Add(figure);
+                line = Console.ReadLine();
+            }
+
+            return figures;
         }
     }
+}
+
+public class Rectangle : Figure
+{
+    public Rectangle(string name, int leftX, int topY, int rightX, int bottomY) : base(name)
+    {
+        LeftX = leftX;
+        TopY = topY;
+        BottomY = bottomY;
+        RightX = rightX;
+    }
+
+    public int LeftX { get; set; }
+
+    public int TopY { get; set; }
+
+    public int BottomY { get; set; }
+
+    public int RightX { get; set; }
+}
+
+public class Circle : Figure
+{
+    public Circle(string name, int centerX, int centerY, int radius) : base(name)
+    {
+        CenterX = centerX;
+        CenterY = centerY;
+        Radius = radius;
+    }
+
+    public int CenterX { get; set; }
+
+    public int CenterY { get; set; }
+
+    public int Radius { get; set; }
+}
+
+public abstract class Figure
+{
+    public Figure(string name)
+    {
+        Name = name;
+        Children = new List<Figure>();
+    }
+
+    public string Name { get; set; }
+
+    public List<Figure> Children { get; private set; }
+
+    public int Depth { get; set; }
+
+    public Figure Successor { get; set; }
 }
